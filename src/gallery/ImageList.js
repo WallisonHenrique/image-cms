f0541eb3f9e3172dc9bedc286e06603ae8304cb7
Wallison Henrique <wallisonhen@gmail.com>
2020-07-14 00:20:43 -0300

    item-img efeito de opacidade

--- src/gallery/ImageList.js
+++ src/gallery/ImageList.js
@@ -8,14 +8,7 @@ class ImageList extends React.Component {
 	}
 
 	handleClick(id, evt) {
-		if (this.state[id]) 
-			this.setState({ ...this.state, [id]: false });
-		else
-			this.setState({ ...this.state, [id]: true });
-	}
-
-	handleChecked(evt) {
-		evt.preventDefault();
+		this.setState({ ...this.state, [id]: this.state[id] ? false : true })
 	}
 
 	render() {
@@ -27,8 +20,10 @@ class ImageList extends React.Component {
 
 		return images.map((img) =>
 			<div key={img.id} className="col-3">
-				<div className={`item-img ${this.state[img.id] ? 'selected' : ''}`} onClick={ () => this.handleClick(img.id)}>
-					<img src={require(`../data/images/${img.path}`)} alt={img.description} title={img.name}/>
+				<div className={`item-img ${this.state[img.id] ? 'item-selected' : ''}`} onClick={ () => this.handleClick(img.id)}>
+					<div className="wrapper-img">
+						<img src={require(`../data/images/${img.path}`)} alt={img.description} title={img.name}/>
+					</div>
 					<input type="checkbox" defaultChecked={ this.state[img.id] } onClick={ (evt)=> evt.preventDefault() } />
 				</div>
 			</div>
