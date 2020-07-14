a2086db69fdca152088201849699f3f6f0012254
Wallison Henrique <wallisonhen@gmail.com>
2020-07-13 23:37:52 -0300

    adicionado o selected da imagem para download

--- src/gallery/ImageList.js
+++ src/gallery/ImageList.js
@@ -3,12 +3,19 @@ import React from 'react';
 class ImageList extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {};
 		this.handleClick = this.handleClick.bind(this);
 	}
 
-	handleClick(evt) {
+	handleClick(id, evt) {
+		if (this.state[id]) 
+			this.setState({ ...this.state, [id]: false });
+		else
+			this.setState({ ...this.state, [id]: true });
+	}
 
-		evt.currentTarget.classList.add('selected');
+	handleChecked(evt) {
+		evt.preventDefault();
 	}
 
 	render() {
@@ -20,9 +27,9 @@ class ImageList extends React.Component {
 
 		return images.map((img) =>
 			<div key={img.id} className="col-3">
-				<div className="item-img" onClick={this.handleClick}>
+				<div className={`item-img ${this.state[img.id] ? 'selected' : ''}`} onClick={ () => this.handleClick(img.id)}>
 					<img src={require(`../data/images/${img.path}`)} alt={img.description} title={img.name}/>
-					<input type="checkbox"/>
+					<input type="checkbox" defaultChecked={ this.state[img.id] } onClick={ (evt)=> evt.preventDefault() } />
 				</div>
 			</div>
 		);
